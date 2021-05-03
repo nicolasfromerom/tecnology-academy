9c8236149fffc969792aeb2fd10f111dc2af04e8
Nicolas Fabricio Romero Morales <romero.nicolasone@gmail.com>
2021-05-03 16:07:09 -0500

    Related courses

--- resources/views/course.blade.php
+++ resources/views/course.blade.php
@@ -4,7 +4,14 @@
         <div class="p-8 bg-gray-200 col-span-1">
             <ul class="flex flex-col">
                 <li class="font-medium text-sm text-gray-400 uppercase mb-4">Contenido</li>
-                ------
+                @foreach ($course->posts as $post)
+                <li class="flex items-center text-gray-600 mt-2">
+                    {{$post->name}}
+                    @if ($post->free)
+                        <span class="text-xs text-gray-500 font-semibold bg-gray-300 rounded-full ml-auto px-2">Gratis</span>
+                    @endif
+                </li>   
+                @endforeach
             </ul>
         </div>
         <div class="text-gray-700 col-span-2">
@@ -17,6 +24,19 @@
                     <p class="text-gray-500 text-sm">{{$course->user->name}}</p>
                     <p class="text-gray-300 text-xs">{{$course->created_at->diffForHumans()}}</p>
                 </div>
+            </div>
+            <div class="grid grid-cols-2 gap-4 my-8">
+                @foreach ($course->similar() as $course)
+                    <div class="bg-white shadow-lg rounded-lg px-4 py-6 text-center">
+                        <a href="{{route('course', $course->slug)}}">
+                            <img src="{{ $course->image }}" alt="Curso" class="rounded-md mb-2">
+                            <h2 class="text-lg text-gray-600 truncate uppercase">{{$course->name}}</h2>
+                            <h3 class="text-md text-gray-500">{{$course->excerpt}}</h3>
+
+                            <img src="{{ $course->user->avatar }}" alt="" class="rounded-full mt-4 mx-auto h-16 w-16">
+                        </a>
+                    </div>
+                @endforeach
             </div>
         </div>
     </div>
